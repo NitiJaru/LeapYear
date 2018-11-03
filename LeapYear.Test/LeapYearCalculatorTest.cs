b41fa6b739e92babd15df9f9396a6d97826059b0
Earn <earndarkness@perfenterprise.com>
2018-11-03 13:31:53 +0700

    refactory der

--- LeapYear.Test/LeapYearCalculatorTest.cs
+++ LeapYear.Test/LeapYearCalculatorTest.cs
@@ -6,52 +6,33 @@ namespace LeapYear.Test
 {
     public class LeapYearCalculatorTest
     {
-        [Theory]
+        [Theory(DisplayName="BeLeapYear")]
         [InlineData(400)]
         [InlineData(2000)]
-        public void DivisibleBy400_ShouldBeLeapYear(int number)
-        {
-            var cal = new LeapYearCalculator();
-            var result = cal.IsLeapYear(number);
-            Assert.True(result);
-        }
-
-        [Theory]
-        [InlineData(1700)]
-        [InlineData(1800)]
-        [InlineData(1900)]
-        [InlineData(2100)]
-        public void DivisibleBy100_ShouldNotBeLeapYear(int number)
-        {
-            var cal = new LeapYearCalculator();
-            var result = cal.IsLeapYear(number);
-            Assert.False(result);
-        }
-
-        [Theory]
         [InlineData(2008)]
         [InlineData(2012)]
         [InlineData(2016)]
         [InlineData(2020)]
-        public void DivisibleBy4AndNotBy100ShouldBeLeapYear(int number)
+        public void ShouldBeLeapYear(int number)
         {
-            // ??? % 4 == 0 && ??? % 100 !=0 /
             var cal = new LeapYearCalculator();
             var result = cal.IsLeapYear(number);
             Assert.True(result);
         }
-
-        [Theory]
+        
+        [Theory (DisplayName="NotBeLeapYear")]
+        [InlineData(1700)]
+        [InlineData(1800)]
+        [InlineData(1900)]
+        [InlineData(2100)]
         [InlineData(2017)]
         [InlineData(2018)]
-        [InlineData(2019)]      
-        public void DivisibleBy4AndNotBy100ShouldBeNotLeapYear(int number)
+        [InlineData(2019)] 
+        public void ShouldNotBeLeapYear(int number)
         {
-            // ??? % 4 == 0 && ??? % 100 !=0 /
             var cal = new LeapYearCalculator();
             var result = cal.IsLeapYear(number);
             Assert.False(result);
         }
-
     }
 }
