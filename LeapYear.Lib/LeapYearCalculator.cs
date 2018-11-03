e894b19b94265a60c271b6094296b9e78e896dd9
Earn <earndarkness@perfenterprise.com>
2018-11-03 12:45:59 +0700

    Implement leap year

---
+++ LeapYear.Lib/LeapYearCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeapYear.Lib
+{
+    public class LeapYearCalculator
+    {
+        public bool IsLeapYear(int year)
+        {
+            // return year % 400 == 0;
+            if (year % 100 == 0 && year % 400 == 0)
+            {
+                return true;
+            }
+            if (year % 4 == 0 && year % 100 !=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
